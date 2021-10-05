df2a03e1b8736565908555f0b31ec9a1a76f3875
Mao Li <ml1911@georgetown.edu>
2021-10-05 13:00:14 -0400

    upload

---
+++ HW3.0/HW3.0A1.py
@@ -0,0 +1,119 @@
+from keras.datasets import boston_housing
+from keras import models
+from keras import layers
+import numpy as np
+
+# split the dataset
+(train_data, train_targets), (test_data, test_targets)=boston_housing.load_data()
+
+# view the shape of both train data and test data
+train_data.shape
+test_data.shape
+
+# normalize the bost datasets
+mean = train_data.mean(axis=0)
+train_data -= mean
+std = train_data.std(axis=0)
+train_data /= std
+test_data -= mean
+test_data /= std
+
+# build the network
+def build_model():
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation='relu',
+    input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64, activation='relu'))
+    # since this is a regression problem, there is no need to use activation in the last layer
+    model.add(layers.Dense(1))
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+    return model
+
+# since the dataset is small, adjust the parameter using K-fold validation
+k=4
+num_val_samples = len(train_data) // k
+num_epochs = 100
+all_scores = []
+
+for i in range(k):
+    print('processing fold #', i)
+    # partition the data
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+    # combine the rest of the data
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
+    # fit the model
+    model = build_model()
+    model.fit(partial_train_data, partial_train_targets,
+              epochs=num_epochs, batch_size=1, verbose=0)
+    val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
+    all_scores.append(val_mae)
+
+# Running this with num_epochs = 100 yields the following results
+np.mean(all_scores)
+
+# training the network a bit longer: 500 epochs
+num_epochs = 500
+all_mae_histories = []
+for i in range(k):
+    print('processing fold #', i)
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis=0)
+    model = build_model()
+    history = model.fit(partial_train_data, partial_train_targets,
+                        validation_data=(val_data, val_targets),
+                        epochs=num_epochs, batch_size=1, verbose=0)
+    mae_history = history.history['val_mae']
+    all_mae_histories.append(mae_history)
+    
+average_mae_history = [np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+
+
+import matplotlib.pyplot as plt
+plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+
+# smooth the curve
+def smooth_curve(points, factor=0.9):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            # Replace each point with an exponential moving average of the previous points,
+            # to obtain a smooth curve.
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
+
+# Omit the first 10 data points, which are on a different scale than the rest of the curve
+smooth_mae_history = smooth_curve(average_mae_history[10:])
+plt.plot(range(1, len(smooth_mae_history) + 1), smooth_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+
+# finally build the model
+model = build_model()
+model.fit(train_data, train_targets,
+          epochs=80, batch_size=16, verbose=0)
+test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
+
+test_mae_score
